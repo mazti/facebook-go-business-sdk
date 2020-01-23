290ec7ab93cbcbedabb51023cd9c3a0c8fc6bac4
Tien Nguyen <tien.nguyenvan@grabtaxi.com>
2020-01-23 14:30:37 +0700

    [cleanup] keys

--- example/adhoc_api_request.go
+++ example/adhoc_api_request.go
@@ -5,11 +5,6 @@ import (
 	"github.com/mazti/facebook-go-business-sdk/sdk"
 )
 
-var (
-	accessToken = "EAAI2nqB1r4MBAJoRlfhMCr1zrDN81PUtt64H9LN5r2I35Q9xOEjLyQSIDWZAqrxJ6U5hpcZAIBFZAZCkpZAeL7C7gczhRZBEl8oe9DSxOjt73rQ4GJHBm8VbuaTvhnRdztl1pp8RzdY8lSJcg3WvEXhP4jIGGCDeASvcHGK5OBCAZDZD"
-	appSecret   = "277fc82bedf9841cef91a15acef02d00"
-)
-
 func Log(a ...interface{}) {
 	fmt.Println(a...)
 }
