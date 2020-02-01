7b364ab8654d5c032f11810099da6e44e77060cd
Tien Nguyen <tien.nguyenvan@grabtaxi.com>
2020-02-01 12:30:10 +0700

    [sdk2] Experimental version 2

---
+++ sdkv2/error/error.go
@@ -0,0 +1,24 @@
+package error
+
+import "net/http"
+
+type FacebookRequestError struct {
+	message string
+	status  string
+	url     string
+	method  string
+}
+
+// TODO: process response
+func NewFacebookRequestError(response *http.Response, method, url string) *FacebookRequestError {
+	return &FacebookRequestError{
+		message: "",
+		status:  response.Status,
+		url:     url,
+		method:  method,
+	}
+}
+
+func (e FacebookRequestError) Error() string {
+	return e.message
+}
